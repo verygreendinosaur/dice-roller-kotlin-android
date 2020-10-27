c817a182e80c9a4d25ed63deb67dc9ab81b5c623
Chris Poole <chris.st.poole@gmail.com>
2020-10-27 14:58:21 -0700

    Update results text

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 private const val ROLL_BUTTON_TEXT = "Let's Roll"
 private const val TOAST_TEXT = "button clicked"
@@ -22,8 +24,15 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.text = ROLL_BUTTON_TEXT
         rollButton.setOnClickListener {
-            Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        var randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        resultText.text = randomInt.toString()
     }
 
 }
