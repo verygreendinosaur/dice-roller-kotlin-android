e8e0da3dd9c75e668d852847225f90f64bbfea07
Chris Poole <chris.st.poole@gmail.com>
2020-10-27 15:08:55 -0700

    Add, assign dice images

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.*
 
 private const val ROLL_BUTTON_TEXT = "Let's Roll"
-private const val TOAST_TEXT = "button clicked"
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,9 +28,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         var randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 
 }
