d2f85d37348a0d235a8f347f9d1d28dd9c6ee752
Chris Poole <chris.st.poole@gmail.com>
2020-10-27 13:51:52 -0700

    Update button from activity

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+private const val ROLL_BUTTON_TEXT = "Let's Roll"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupButtons()
     }
+
+    private fun setupButtons() {
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = ROLL_BUTTON_TEXT
+    }
+
 }
