6fc35e79c5f03f10ab3d7688ab01ab0ce46d1a04
Chris Poole <chris.st.poole@gmail.com>
2020-10-27 14:41:45 -0700

    Add button click listener

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 private const val ROLL_BUTTON_TEXT = "Let's Roll"
+private const val TOAST_TEXT = "button clicked"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +19,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupButtons() {
         val rollButton: Button = findViewById(R.id.roll_button)
+
         rollButton.text = ROLL_BUTTON_TEXT
+        rollButton.setOnClickListener {
+            Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
